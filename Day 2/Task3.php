df409a7937ec1bb89dc0bbd70880152776658bf0
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-16 00:04:30 +0500

    Update Task3.php

--- Day 2/Task3.php
+++ Day 2/Task3.php
@@ -8,25 +8,21 @@
 <body>
     
 	<?php
-	    $myarray = array(2 , 2, 3, 4 ,5 ,6 , 7 , 8 , 9);
+	$myarray = array(2 , 2, 3, 4 ,5 ,6 , 7 , 8 , 9);
 
         foreach($myarray as $index)
          echo $index . "<br>";
 
          echo "=======================================<br>";
-$num = 0;
+	$num = 0;
         $len = count($myarray);
-         while($num <  $len-1)
-         {
-            
+         while ($num <  $len-1) {
           echo $myarray[$num] . "<br>";
           $num++;
          }
 
-
 	?>
-	
-	
+		
 </body>
 
 </html>
