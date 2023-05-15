99764372fe2fbceaab26111ab068fb943f1965f4
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-16 00:01:34 +0500

    Update Task1.php

--- Day 2/Task1.php
+++ Day 2/Task1.php
@@ -8,10 +8,9 @@
 <body>
     
 	<?php
-	    $myarray = array(1,2,2,4,5,6,7,8,9);
-        $len = count($myarray);
-        for($i = 1 ; $i <= $len ; $i++)
-        {
+	$myarray = array(1,2,2,4,5,6,7,8,9);
+        $len     = count($myarray);
+        for ($i = 1 ; $i <= $len ; $i++) {
             if($i % 3 == 1)
             echo "[ ";
             echo $myarray[$i-1] . " ";
@@ -22,8 +21,7 @@
         
         echo "<br><br>=========================================<br><br>";
         $j = 0;
-        for($i = 0; $i < $len ; $i++)
-        {
+        for ($i = 0; $i < $len ; $i++) {
             echo "[ ";
             for($j = $i%3; $j < 3 ; $j++ )
              echo $myarray[$j] . " ";
@@ -31,7 +29,6 @@
         }
 
 	?>
-	
 	
 </body>
 
