7be5357987916608e6daa8fb855e022f19d1ccde
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-21 00:12:42 +0500

    Add files via upload

---
+++ lvariable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<htmml>
+
+<head>
+    <title>PHP </title>
+</head>
+
+<body>
+    
+	<?php
+	$text = "My Name is Asim Suhail <br>";
+	$number1 = 12345;
+	$number2 = 56789;
+	
+	$num = $number1 + $number2;
+	
+	echo $text . $num;
+	?>
+	
+	
+</body>
+
+</html>
