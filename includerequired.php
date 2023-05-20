3b6afa654d9ee1a6ddcfb7e008d0a075671ad65f
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-20 23:34:07 +0500

    Add files via upload

---
+++ includerequired.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<htmml>
+
+<head>
+    <title>PHP </title>
+</head>
+
+<body>
+    
+	<table>
+        <tr>
+            <th>Name</th>
+            <th>Class</th>
+            <th>age</th>
+
+        </tr>
+
+        <td>Asim</td>
+        <td>11th</td>
+        <td>28</td>
+
+        <td>Hamid</td>
+        <td>11th</td>
+        <td>28</td>
+    </table>
+	
+	
+</body>
+
+</html>
