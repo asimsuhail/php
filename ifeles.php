4d1911173b3a276fd72e11b36eb9a20ba39d4c94
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-20 23:33:20 +0500

    Add files via upload

---
+++ ifeles.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<htmml>
+
+<head>
+    <title>PHP </title>
+</head>
+
+<body>
+    
+	<?php
+	    $number1 = 13;
+        $number2 = 13;
+
+        if($number1 == 13)
+         echo "condition is true<br>";
+        else
+         echo "condition is false<br>";
+
+        
+
+
+	?>
+	
+	
+</body>
+
+</html>
