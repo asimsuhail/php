378e06fcfb7a2733471ff28db66fb413b830ee4b
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-23 23:41:19 +0500

    Add files via upload

---
+++ phparray.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<htmml>
+
+<head>
+    <title>PHP </title>
+</head>
+
+<body>
+    
+	<?php
+	    $city = array("Islamabad is in pakistan","Mumbai is in India","Dhaka is in Bangladesh","Riadh is in KSA");
+
+        echo "<b>where is Islamabad?</b><br>" . $city[0] . "<br>";
+        echo "<b>where is Mumbai?</b><br>" . $city[1] . "<br>";
+        echo "<b>where is Dhaka?</b><br>" . $city[2] . "<br>";
+        echo "<b>where is Riyadh?</b><br>" . $city[3] . "<br>";
+
+
+	?>
+	
+	
+</body>
+
+</html>
