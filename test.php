d486f039e795829e233df356247f085f42a2f279
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-23 23:42:35 +0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<htmml>
+
+<head>
+    <title>PHP </title>
+</head>
+
+<body>
+    
+	<?php
+	
+	 echo "Hello MY name is Asim <br> I am a web designer<br> I am learning to code for last 4 years<br>";
+	 $fname = "Asim";
+	 $lname = "Suhail";
+	 echo "<i>Ouptut Like:". $fname ." ". $lname."</i>";
+	 echo "
+	 <table width='600' bgcolor='orange' align='center' border='2'>
+	 
+	    <tr align='center'>
+		  <td>Name</td>
+		  <td>F /Name</td>
+		  <td>Class</td>
+		  <td>Age</td>
+		  <td>Timing</td>
+		</tr>
+		
+		
+		<tr align='center'>
+		  <td>Name</td>
+		  <td>F /Name</td>
+		  <td>Class</td>
+		  <td>Age</td>
+		  <td>Timing</td>
+		</tr>
+		
+		<tr align='center'>
+		  <td>Name</td>
+		  <td>F /Name</td>
+		  <td>Class</td>
+		  <td>Age</td>
+		  <td>Timing</td>
+		</tr>
+		<tr align='center'>
+		  <td>Name</td>
+		  <td>F /Name</td>
+		  <td>Class</td>
+		  <td>Age</td>
+		  <td>Timing</td>
+		</tr>
+		</table>";
+	?>
+	
+	
+</body>
+
+</html>
