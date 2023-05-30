7f2401378ffffc5693c8e292ac808ad87c9e9d92
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-30 23:41:27 +0500

    Update loops.php

--- loops.php
+++ loops.php
@@ -8,21 +8,20 @@
 <body>
     
 	<?php
-	    $num = 10;
-        while($num--)
+	$num = 10;
+        while( $num-- ) {
          echo $num . "<br>";
-
-         for($num=1988; $num <= 2023; $num++)
-          echo $num . "<br>";
-
+	}
+        for($num=1988; $num <= 2023; $num++) {
+         echo $num . "<br>";
+	}
+	
         $names = array("asim" , "suhail" );
 
-        foreach($names as $val)
+        foreach($names as $val) {
          echo $val . "<br>";
-
+	}
         
-
-
 	?>
 	
 	
